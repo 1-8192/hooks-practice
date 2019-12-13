845e89f9c54b91606b33b779fe0cbfb13c39847a
1-8192 <allegranzia@gmail.com>
2019-12-13 11:41:11 -0500

    click counter using setState hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import ComponentClass from './ComponentClass.js';
-
-function Box(props) {
-  return (
-    <div>
-      <h1>{props.text}</h1>
-      <h2>{props.subtitle}</h2>
-    </div>
-  )
-}
 
 function App() {
+
+  //takes initial state as argument, returns that value as first element in array. 2nd value is function to update state value. Cannot invoke useState within conditional. Can assign values using ES6 destructuring. Value that corresponds to function will be update function name.
+  const [count, setCount] = useState(0)
+
+  //onclick is calling the update function declared for the hooks
+  const handleClick = () => setCount(count + 1)
+
   return (
     <div>
-      <Box text="text from the props" subtitle="Another prop"/>
-      <ComponentClass active='ON' inactive='OFF'/>
+      <button onClick={handleClick}>Increase</button>
+      <h2>{count}</h2>
     </div>
   );
 }
